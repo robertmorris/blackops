a8178d2d2ab65d884becaec5f7e674581efeac60
Ryan Morris <rmorris@evestment.com>
2018-10-23 17:12:10 -0400

    add gamertag input form

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,30 @@
 <template>
-  <div class="home" style="background-color: black;">
+
+<div id="inputForm">
+  <p class="thick">Xbox Live Gamertag</p>
+    <b-form-input v-model="text1"
+                  type="text"
+                  placeholder="Enter your Xbox Gamertag">
+    </b-form-input>
+    <b-button class="mt-2">Add</b-button>
+</div>
+
+
+
+<!-- test stuff -->
+
+<!--   <div>
+    <div class="input-group">
+      <input type="text" class="form-control">
+      <span class="input-group-btn">
+        <button class="btn btn-default" type="button">Go!</button>
+      </span>
+    </div>
+  </div> -->
+
+
+
+<!--   <div class="home" style="background-color: black;">
     <div class="home" style="background-color: black;">
       <b-container>
         <b-jumbotron header="Old Man Clan">
@@ -7,7 +32,7 @@
         </b-jumbotron>
       </b-container>
     </div>
-  </div>
+  </div> -->
 </template>
 
 <script>
@@ -18,6 +43,23 @@ export default {
   name: 'home',
   components: {
     HelloWorld
+  },
+
+    data () {
+    return {
+      text1: ''
+    }
   }
 }
 </script>
+
+<style>
+#inputForm {
+  margin: auto;
+  width: 25%;
+}
+
+p.thick {
+    font-weight: bold;
+}
+</style>
